de189282e3e5a9115bc714264f95a2cfa1cdd675
idk_man <72506130+Killua-Gon-Kite@users.noreply.github.com>
2022-05-24 12:01:35 +0530

    commentsremovedbutotherbranch
    okvro

--- something.py
+++ something.py
@@ -3,12 +3,9 @@ import sys, pygame
 
 pygame.init()
   
-# Initializing surface
 surface = pygame.display.set_mode((1200, 1800))
   
-# Initialing Color
 color = (255, 255, 255)
-  
-# Drawing Rectangle
+
 pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
 pygame.display.flip()
