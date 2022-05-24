e0494911b66b8ca4c519ee4522fb493fdbdf84c2
wellyesbutno <ak57as13@gmail.com>
2022-05-24 12:57:37 +0530

    Run something.py to see a red rectange

---
+++ settings.py
@@ -0,0 +1,12 @@
+#Constants
+HEIGHT = WIDTH = 800
+DISPLAY = (HEIGHT, WIDTH)
+TITLE = "Chess Card Game"
+FPS = 60
+
+#Colours
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
--- something.py
+++ something.py
@@ -1,15 +1,26 @@
-import random
-import sys, pygame
+from tkinter import W
+import pygame as pg
+from settings import *
 
-pygame.init()
-  
-surface = pygame.display.set_mode((1200, 1800))
-  
-color = (255, 255, 255)
+pg.init()
+clock = pg.time.Clock()
 
-surface = pygame.display.set_mode((1200, 1800))
-  
-color = (255, 255, 255)
-  
-pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
-pygame.display.flip()
+#Initialize screen
+screen = pg.display.set_mode(DISPLAY)
+pg.display.set_caption(TITLE)
+
+running = True
+
+while running:
+    # Set FPS
+    clock.tick(FPS)
+
+    #Check for events
+    for ev in pg.event.get():
+        if ev.type == pg.QUIT:
+            running = False
+            pg.quit()
+
+    screen.fill(BLACK)
+    pg.draw.rect(screen, RED, (WIDTH/2, HEIGHT/2, 64, 64))
+    pg.display.flip()
