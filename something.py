5f9cb8d775d567d67cc3b3bd140de55ff571b48a
idk_man <72506130+Killua-Gon-Kite@users.noreply.github.com>
2022-05-24 11:53:52 +0530

    Update something.py

--- something.py
+++ something.py
@@ -1,3 +1,14 @@
 import random
 import sys, pygame
 
+pygame.init()
+  
+# Initializing surface
+surface = pygame.display.set_mode((1200, 1800))
+  
+# Initialing Color
+color = (255, 255, 255)
+  
+# Drawing Rectangle
+pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
+pygame.display.flip()
